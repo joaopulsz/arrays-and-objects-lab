927d2ed9d7d96bd0d0ffd0fb5a4cdb7cc04e7132
Joao Pedro Canale Pulsz <joaopulsz@hotmail.com>
2022-09-20 15:53:18 +0100

    finished extension

--- advanced.js
+++ advanced.js
@@ -3,7 +3,7 @@
  */
 
 
-unitedKingdom = [
+const unitedKingdom = [
     {
         name: "Scotland",
         capital: "Edinburgh",
@@ -38,23 +38,32 @@ unitedKingdom = [
 ]
 
 
+// I came up with two alternative solutions for questions 1 and 2.
+
+
 /**
  * Q1. Add some tourist attractions to England
  */
+const newArr = unitedKingdom.map(country => country.name === "England" ? {...country, touristAttractions: ["London Eye", "Buckingham Palace"]} : country);
+console.log(newArr);
 
-england = null;
+unitedKingdom[1].touristAttractions = ["London Eye", "Buckingham Palace"];
+const england = unitedKingdom[1];
 
-// console.log(england);
+console.log(england);
 
 
 
 /**
  * Q2. Change the capital of Wales to "Cardiff"
  */
+ const newArr2 = unitedKingdom.map(country => country.name === "Wales" ? {...country, capital: "Cardiff"} : country);
+ console.log(newArr2);
 
-wales = null;
+unitedKingdom[2].capital = "Cardiff";
+const wales = unitedKingdom[2];
 
-// console.log(wales);
+console.log(wales);
 
 
 
@@ -62,12 +71,24 @@ wales = null;
  * Q3. Find a method in the documentation which will give you all of the available keys for Northern Ireland
  */
 
-northernIrelandKeys = null;
+northernIrelandKeys = Object.keys(unitedKingdom[3]);
 
-// console.log(northernIrelandKeys);
+console.log(northernIrelandKeys);
 
 
 
 /**
  * Q4. Use an if statement to compare the population of Scotland to the other countries and print out if it is the biggest, smallest or somewhere in the middle.
  */
+
+if (unitedKingdom[0].population > unitedKingdom[1].population &&
+    unitedKingdom[0].population > unitedKingdom[2].population &&
+    unitedKingdom[0].population > unitedKingdom[3].population) {
+        console.log("Biggest");
+    } else if (unitedKingdom[0].population < unitedKingdom[1].population &&
+        unitedKingdom[0].population < unitedKingdom[2].population &&
+        unitedKingdom[0].population < unitedKingdom[3].population) {
+            console.log("Smallest");
+        } else {
+            console.log("In the middle")
+        }
